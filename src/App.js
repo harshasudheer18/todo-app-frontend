128ab164619d2ecf7513e53785e58586d5aa48e0
harshasudheer18 <harsha.sudheer@thoughtworks.com>
2024-04-10 12:17:17 +0530

    Add. Route for Login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/login/Login';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Login/>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
     </div>
   );
 }
