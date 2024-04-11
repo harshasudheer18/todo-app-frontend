1b00abc5bfbf14b2652af34355be01ade0758e42
harshasudheer18 <harsha.sudheer@thoughtworks.com>
2024-04-11 21:20:39 +0530

    Add. Search component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,17 +1,18 @@
 import React, {useState} from "react";
 import AddTask from "./addtask/AddTask";
+import Search from "./search/Search";
 import EditTask from "./edittask/EditTask";
 import User from "../../assets/images/icons/user.svg";
 import Logo from "../../assets/images/logo.svg";
 import Add from "../../assets/images/icons/add.svg";
-import Search from "../../assets/images/icons/search.svg";
+import SearchIcon from "../../assets/images/icons/search.svg";
 import Today from "../../assets/images/icons/today.svg";
 import Upcoming from "../../assets/images/icons/upcoming.svg";
 import Project from "../../assets/images/icons/project.svg";
 import "./Dashboard.css";
 
 const Dashboard = () => {
-  const [seletedOption, setSeletedOption] = useState({add: false, edit: false});
+  const [seletedOption, setSeletedOption] = useState({add: false, edit: false, search: false});
 
   const handleOptionSelect = (option) => {
     let currentOption = {...seletedOption}
@@ -30,11 +31,11 @@ const Dashboard = () => {
             <img src={Add} alt="add" />
             <p>Add task</p>
           </div>
-          <div className={seletedOption.edit ? "sidebar-button sidebar-button-selected" : "sidebar-button"} onClick={() => handleOptionSelect("edit")}>
-            <img src={Search} alt="search" />
+          <div className={seletedOption.search ? "sidebar-button sidebar-button-selected" : "sidebar-button"} onClick={() => handleOptionSelect("search")}>
+            <img src={SearchIcon} alt="search" />
             <p>Search</p>
           </div>
-          <div className="sidebar-button">
+          <div className={seletedOption.edit ? "sidebar-button sidebar-button-selected" : "sidebar-button"} onClick={() => handleOptionSelect("edit")}>
             <img src={Today} alt="today" />
             <p>Today</p>
           </div>
@@ -51,6 +52,7 @@ const Dashboard = () => {
       </div>
       <div className="main-container">
         {seletedOption.add && <AddTask/>}
+        {seletedOption.search && <Search/>}
         {seletedOption.edit && <EditTask/>}
       </div>
       <div className="user-container">
---
+++ src/components/dashboard/search/Search.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Dropdown from "../../../assets/images/icons/dropdown.svg";
+import SearchIcon from "../../../assets/images/icons/search-black.svg";
+import Clock from "../../../assets/images/icons/clock.svg";
+import Home from "../../../assets/images/icons/home.svg";
+import Today from "../../../assets/images/icons/today-grey.svg";
+import Upcoming from "../../../assets/images/icons/upcoming-grey.svg";
+import "./Search.css";
+
+const Search = () => {
+  return (
+    <div className="search-container">
+      <div className="search-inner-container">
+        <div className="search-bar">
+          <button className="filter-button">
+            <p>Filter by</p>
+            <img className="search-dropdown" src={Dropdown} alt="dropdown" />
+          </button>
+          <input
+            className="search-input"
+            type="text"
+            name="search"
+            placeholder="Search"
+          />
+          <img
+            src={SearchIcon}
+            alt="dropdown"
+            style={{ padding: "12px 16px 12px 12px" }}
+          />
+        </div>
+        <div className="links-container">
+          <p style={{color: "#767676", paddingLeft: "16px", paddingTop: "8px"}}>Recent searches</p>
+          <div className="links-item">
+            <img src={Clock} alt="clock" />
+            <p>Inbox</p>
+          </div>
+          <div className="links-item">
+            <img src={Clock} alt="clock" />
+            <p>Work</p>
+          </div>
+        </div>
+        <div className="links-container">
+          <p style={{color: "#767676", paddingLeft: "16px", paddingTop: "8px"}}>Navigation</p>
+          <div className="links-item">
+            <img src={Home} alt="home" />
+            <p>Home</p>
+          </div>
+          <div className="links-item">
+            <img src={Today} alt="today" />
+            <p>Today</p>
+          </div>
+          <div className="links-item">
+            <img src={Upcoming} alt="upcoming" />
+            <p>Upcoming</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
